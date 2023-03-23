305dac36d9778f7bd2e3966837a268e39162c82e
Gudykson <127319413+Gudykson@users.noreply.github.com>
2023-03-23 12:27:34 +0000

    proba mozliwosci wyboru n

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 import java.util.Random;
+import java.util.*;
+class UserInputDemo
+  {
 class Main {
   public static void main(String[] args) {
     //double suma = 0;
     double srednia = 0;
     double max = 0;
-    int n =1000;
+    Scanner sc= new Scanner(System.in);   
+    System.out.print("Wpisz n");  
+
+    int n =sc.nextInt();
     for(int i=0;i<n;i++){
       Random rand = new Random();
         int upperbound = 100;
       double a = rand.nextDouble(upperbound);
-      //suma = suma + Math.pow(a,2);
+     
       //System.out.println("Wygenerowana liczba nr." + (i + 1) +  ": " + a);
       srednia = srednia + a/n;
       if (a>max) 
@@ -19,7 +25,8 @@ class Main {
       
     }
     System.out.println("średnia wynosi " + srednia);
-    System.out.println("max wynosi " + max);
-    //System.out.println("Suma kwadratow tych " + n + " liczb losowych wynosi " + suma);
+    //System.out.println("max wynosi " + max);
+   
   }
 }
+  }
